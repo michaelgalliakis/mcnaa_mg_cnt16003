d97cd07d1917c99c36e8bc1a4671000089a9c75b
Michael <michaelgalliakis@yahoo.gr>
2017-05-21 17:21:53 +0300

    First commit

---
+++ FindCellTower_mg_cnt16003/app/src/main/java/mg/mcnaa/findcelltower/findcelltower_mg_cnt16003/Main.java
@@ -0,0 +1,13 @@
+package mg.mcnaa.findcelltower.findcelltower_mg_cnt16003;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
